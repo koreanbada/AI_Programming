1c9880b2cfdec2d09324758a9a16c3baf14a40a5
sun <koreanbada@gmail.com>
2018-02-07 17:13:07 +0900

    2
    2

--- Assets/3Part/Script/PlayerTank.cs
+++ Assets/3Part/Script/PlayerTank.cs
@@ -4,11 +4,10 @@ using System.Collections;
 public class PlayerTank : MonoBehaviour 
 {
     public Transform targetTransform;
-    //private float movementSpeed, rotSpeed;
+    //private float movementSpeed, rotSpeed;//좀비할 때 사용
     private float movementSpeed;
     private float rotSpeed;
 
-    // Use this for initialization
     void Start () 
     {
         movementSpeed = 10.0f;
@@ -16,19 +15,22 @@ public class PlayerTank : MonoBehaviour
         targetTransform = GameObject.Find("Target").transform;
 	}
 	
-	// Update is called once per frame
 	void Update () 
     {
-        if(Vector3.Distance(transform.position, targetTransform.position) < 5.0f)
+        if (Vector3.Distance(transform.position, targetTransform.position) < 2.5f)//사거리를 5로 잡음
+        {
             return;
+        }
 
-        Vector3 tarPos = targetTransform.position;
+        Vector3 tarPos = targetTransform.position;//타겟이 5보다 작으면 동작이 안함
         tarPos.y = transform.position.y;
         Vector3 dirRot = tarPos - transform.position;
 
         Quaternion tarRot = Quaternion.LookRotation(dirRot);
         transform.rotation = Quaternion.Slerp(transform.rotation, tarRot, rotSpeed * Time.deltaTime);
+        //선형 보간해 준다. Slerp만큼.
 
         transform.Translate(new Vector3(0, 0, movementSpeed * Time.deltaTime));
+        //new Vector3 없어도 됨.
 	}
 }
