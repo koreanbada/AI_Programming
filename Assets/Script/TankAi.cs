548d06f8d07989f199eed3241608775bcec52f5f
sun <koreanbada@gmail.com>
2018-02-07 12:16:08 +0900

    1
    1

---
+++ Assets/Script/TankAi.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankAi : MonoBehaviour {
+
+    // General state machine variables 범용 상태 기계 변수
+    private GameObject player;
+    private Animator animator;
+    private Ray ray;
+    private RaycastHit hit;
+    private float maxDistanceToCheck = 6.0f;
+    private float currentDistance;
+    private Vector3 checkDirection;
+
+    // Patrol state variables Patrol 상태 변수
+    public Transform pointA;
+    public Transform pointB;
+    public UnityEngine.AI.NavMeshAgent navMeshAgent;
+    
+    private int currentTarget;
+    private float distanceFromTarget;
+    private Transform[] waypoints = null;
+
+    private void Awake() {
+        player = GameObject.FindWithTag("Player");
+        animator = gameObject.GetComponent<Animator>();
+        pointA = GameObject.Find("p1").transform;
+        pointB = GameObject.Find("p2").transform;
+        navMeshAgent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        waypoints = new Transform[2] {
+            pointA,
+            pointB
+        };
+        currentTarget = 0;
+        navMeshAgent.SetDestination(waypoints[currentTarget].position);
+    }
+
+    private void FixedUpdate() {
+        //First we check distance from the player 일단 플레이어와의 거리를 검사한다.
+        currentDistance = Vector3.Distance(player.transform.position, transform.position);
+        animator.SetFloat("distanceFromPlayer", currentDistance);
+
+        //Then we check for visibility 그런 다음 시야에 들어왔는지 확인한다. 
+        checkDirection = player.transform.position - transform.position;
+        ray = new Ray(transform.position, checkDirection);
+        if (Physics.Raycast(ray, out hit, maxDistanceToCheck)) {
+            if(hit.collider.gameObject == player){
+                animator.SetBool("isPlayerVisible", true);
+            } else {
+                animator.SetBool("isPlayerVisible", false);
+            }
+        } else {
+            animator.SetBool("isPlayerVisible", false);
+        }
+
+        //Lastly, we get the distance to the next waypoint target
+        distanceFromTarget = Vector3.Distance(waypoints[currentTarget].position, transform.position);
+        animator.SetFloat("distanceFromWaypoint", distanceFromTarget);
+    }
+
+    public void SetNextPoint() {
+        switch (currentTarget) {
+            case 0:
+                currentTarget = 1;
+                break;
+            case 1:
+                currentTarget = 0;
+                break;
+        }
+        navMeshAgent.SetDestination(waypoints[currentTarget].position);
+    }
+}
---
+++ Assets/Script/TankPatrolState.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankPatrolState : StateMachineBehaviour
+{
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //OnStateEnter는 상태 전이가 시작될때 호출되고 상태기계는 이 상태 평가를 시작한다. 
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("We have entered the patrol state");
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //OnStateUpdate는 OnStateEnter와 OnStateExit 콜백 사이 Update 프레임마다 호출된다. 
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("OnStateUpdate");
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("OnStateExit");
+    }
+
+
+    override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("OnStateMove");
+    } 
+
+    override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Debug.Log("OnStateIK");
+    }
+}
