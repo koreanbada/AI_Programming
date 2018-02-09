791a886d7d956c9db3ea85781990b98a2c70a69c
sun <koreanbada@gmail.com>
2018-02-09 17:02:19 +0900

    2
    2

---
+++ Assets/5Part/Scripts/FireController.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireController : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public Transform firePos;
+
+    void Update()
+    {
+      
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+    void Fire()
+    {
+        StartCoroutine(this.CreateBullet());
+    }
+    IEnumerator CreateBullet() { 
+        Instantiate(bullet, firePos.position, firePos.rotation);
+        yield return null;
+    }
+}
---
+++ Assets/5Part/Scripts/WallController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour {
+
+    public GameObject ExplosionMobile;
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.collider.tag =="BULLET")
+        {
+            Instantiate(ExplosionMobile, transform.position, Quaternion.identity);
+            Destroy(col.gameObject);//총알만 삭제됨
+            //Destroy(this.gameObject);//맞은 벽까지 삭제됨
+        }
+    }
+}
